0b99fd27c1d357502fc0f32378967f5a009b3dba
bekahlundy <lundyrebekah@gmail.com>
2017-01-25 14:34:19 -0700

    jokes are finally displaying on the page woho

--- app/components/JokeContainer/JokeContainer.js
+++ app/components/JokeContainer/JokeContainer.js
@@ -7,34 +7,43 @@ export default class JokeContainer extends React.Component{
     super()
     this.state = {
       joke: '',
+      jokes: [],
       num: 0
     }
   }
-  componentDidMount() {
-    fetch('http://api.icndb.com/jokes/random')
-      .then((data) => data.json())
-      .then((scrub) => this.APIScrubber(scrub.value.joke))
-      .then((joke) => this.setState({ joke: joke }))
-  }
-
-  APIScrubber(api){
-   let scrubbed = api.replace(/&quot;/g, '"');
-   return scrubbed;
- }
+  // componentDidMount() {
+  //   fetch('http://api.icndb.com/jokes/random/')
+  //     .then((data) => data.json())
+  //     .then((scrub) => this.APIScrubber(scrub.value.joke))
+  //     .then((joke) => this.setState({ joke: joke }))
+  // }
 
    getJokes() {
-     console.log('joke')
-   }
+     fetch(`http://api.icndb.com/jokes/random/${this.state.num}?escape=javascript`)
+        .then((response) => response.json())
+        .then((data) => data.value.map(obj => obj.joke))
+        .then(array => this.setState({ jokes: array })
+        )
+        console.log(this.state.num)
+      }
 
+  //  APIScrubber(api){
+  //    let scrubbed = api.replace(/&quot;/g, '"');
+  //    return scrubbed;
+  //  }
    handleChange(e) {
      this.setState({ num: e.target.value })
    }
 
 
     render() {
+      let welcome = (<div>Click to get jokes!</div>)
+      let display = this.state.num === 0 ? welcome : this.state.jokes.map(joke => console.log(joke))
+      // let jokes = this.state.num ===  0 ? welcome : this.state.jokes.map(joke => console.log(joke))
       return(
         <div>
-          <h1>{this.state.joke}</h1>
+          <div>{display}</div>
+          <h1>{this.state.joke}</h1>|
           <Button
             className='get-jokes-btn'
             onClick={this.getJokes.bind(this)}
@@ -48,6 +57,7 @@ export default class JokeContainer extends React.Component{
             className='favorites-btn'
             onClick={() => console.log('favorites')}
             text='Favorites'/>
+          <h1>{this.state.jokes}</h1>
         </div>
       )
   }
