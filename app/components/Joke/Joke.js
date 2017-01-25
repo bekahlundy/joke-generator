507374bc57c66cdcc77f9c5dfaa6afe655c46c34
bekahlundy <lundyrebekah@gmail.com>
2017-01-24 17:18:24 -0700

    random joke now shows up on page reload

--- app/components/Joke/Joke.js
+++ app/components/Joke/Joke.js
@@ -7,9 +7,24 @@ export default class Joke extends React.Component{
       joke: ''
     }
   }
+  componentDidMount() {
+    fetch('http://api.icndb.com/jokes/random')
+      .then((data) => data.json())
+      .then((scrub) => this.APIScrubber(scrub.value.joke))
+      .then((joke) => this.setState({ joke: joke }))
+  }
+
+  APIScrubber(api){
+   let scrubbed = api.replace(/&quot;/g, '"');
+   return scrubbed;
+ }
+
+
     render() {
       return(
-        <div>joke</div>
+        <div>
+          <h1>{this.state.joke}</h1>
+        </div>
       )
   }
 }
