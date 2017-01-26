830fd12c6676908c296d1d79d45f5851e0368651
bekahlundy <lundyrebekah@gmail.com>
2017-01-26 15:39:27 -0700

    taking last name and first name in state

--- app/components/Settings/Settings.js
+++ app/components/Settings/Settings.js
@@ -1,36 +1,52 @@
 import React from 'react';
 import Button from '../Button/Button';
 import Input from '../Input/Input';
-const Settings = () => {
-  return (
-    <div>
-      <p>Set Name:</p>
-      <Input
-        className='set-name-input'
-        placeholder='Set Name'
-        onChange={() => console.log('tip')}/>
-      <Button
-        className='set-btn'
-        text='Set'
-        onClick={() => console.log('set')}/>
-      <Button
-        className='reset-btn'
-        text='Reset'
-        onClick={() => console.log('reset')}/>
-      <p>Parental Controls</p>
-      ON<Input
-        name='parental'
-        className='on-btn'
-        type='radio'
-        onChange={() => console.log('on')}/>
-        OFF<Input
-          name='parental'
-          className='off-btn'
-          type='radio'
-          defaultChecked={true}
-          onChange={() => console.log('off')}/>
-    </div>
-  )
+export default class Settings extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      firstName: '',
+      lastName: ''
+    }
+  }
+  setName(e) {
+    this.setState({ name: e.target.value })
+  }
+  clickSet() {
+    let name = this.state.name
+    this.setState({ firstName: name.split(" ")[0] })
+    this.setState({ lastName: name.split(" ")[1] })
+  }
+  render() {
+    return (
+      <div>
+        <p>Set Name:</p>
+        <Input
+          className='set-name-input'
+          placeholder='Set Name'
+          onChange={this.setName.bind(this)}/>
+          <Button
+            className='set-btn'
+            text='Set'
+            onClick={this.clickSet.bind(this)}/>
+            <Button
+              className='reset-btn'
+              text='Reset'
+              onClick={() => console.log('reset')}/>
+              <p>Parental Controls</p>
+              ON<Input
+                name='parental'
+                className='on-btn'
+                type='radio'
+                onChange={() => console.log('on')}/>
+                OFF<Input
+                  name='parental'
+                  className='off-btn'
+                  type='radio'
+                  defaultChecked={true}
+                  onChange={() => console.log('off')}/>
+                </div>
+              )
+  }
 }
-
-export default Settings;
